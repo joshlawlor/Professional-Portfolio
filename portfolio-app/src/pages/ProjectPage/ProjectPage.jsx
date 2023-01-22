2215bd21a2bb731c2ef5f5ca5350af13ea66d349
Josh Lawlor <joshlawlor1@gmail.com>
2023-01-21 23:06:32 -0500

    Added little styling changes, content added for project folders. Resume page next

--- portfolio-app/src/pages/ProjectPage/ProjectPage.jsx
+++ portfolio-app/src/pages/ProjectPage/ProjectPage.jsx
@@ -7,17 +7,37 @@ import github from "../../assets/images/githubicon.png";
 import email from "../../assets/images/email.png";
 import folder from "../../assets/images/folder.png";
 
-import bugbuddy from "../../assets/images/bugbuddy1.png";
+import bugbuddy1 from "../../assets/images/bugbuddy1.png";
+import bugbuddy2 from "../../assets/images/bugbuddy2.png";
+import bugbuddy3 from "../../assets/images/bugbuddy3.png";
+import bugbuddy4 from "../../assets/images/bugbuddy4.png";
+import bugbuddy5 from "../../assets/images/bugbuddy5.png";
+
+import bug1 from "../../assets/images/bug1.png";
+import bug2 from "../../assets/images/bug2.png";
+import bug3 from "../../assets/images/bug3.png";
+import bug4 from "../../assets/images/bug4.png";
+import bug5 from "../../assets/images/bug5.png";
+
+import movie1 from "../../assets/images/movie1.png";
+import movie2 from "../../assets/images/movie2.png";
+import movie3 from "../../assets/images/movie3.png";
+import movie4 from "../../assets/images/movie4.png";
+
+import portfolio1 from "../../assets/images/portfolio1.png";
+import portfolio2 from "../../assets/images/portfolio2.png";
+import portfolio3 from "../../assets/images/portfolio3.png";
+import portfolio4 from "../../assets/images/portfolio4.png";
+import portfolio5 from "../../assets/images/portfolio5.png";
 
 //BOOTSTRAP
-import { Card, Carousel, Modal, Container } from "react-bootstrap";
+import { Card, Carousel, Modal, Container, Accordion } from "react-bootstrap";
 
 function ProjectPage() {
   const [p1Show, set1Show] = React.useState(false);
   const [p2Show, set2Show] = React.useState(false);
   const [p3Show, set3Show] = React.useState(false);
   const [p4Show, set4Show] = React.useState(false);
-
 
   return (
     <div className="projectMain">
@@ -86,7 +106,7 @@ function ProjectPage() {
         </div>
       </div>
 
-        {/***************** PROJECT ONE MODAL *****************/}
+      {/***************** PROJECT ONE MODAL *****************/}
       <Modal
         className="projectModal"
         show={p1Show}
@@ -96,36 +116,36 @@ function ProjectPage() {
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title className="nintendo-font">Bugbuddy 1.0</Modal.Title>
+          <Modal.Title className="nintendo-font"> Bugbuddy 1.0</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
           <Container className="projectContainer">
             <Card>
-              <Card.Header>
-                <Carousel>
-                  <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
-                  </Carousel.Item>
-                </Carousel>
-              </Card.Header>
-              <Card.Title>BugBuddy</Card.Title>
+            <Card.Body>
+                <Card.Title>A debugging forum app!</Card.Title>
+                <Card.Subtitle><a href="https://github.com/joshlawlor/Debugger-App">Github Repository</a> </Card.Subtitle>
+            </Card.Body>
+            <Carousel>
+                <Carousel.Item>
+                  <img src={bugbuddy1} alt="1"></img>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={bugbuddy2} alt="1"></img>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={bugbuddy4} alt="1"></img>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img src={bugbuddy5} alt="1"></img>
+                </Carousel.Item>
+              </Carousel>
             </Card>
           </Container>
         </Modal.Body>
       </Modal>
 
-
-    {/***************** PROJECT TWO MODAL *****************/}
+      {/***************** PROJECT TWO MODAL *****************/}
 
       <Modal
         className="projectModal"
@@ -142,29 +162,33 @@ function ProjectPage() {
         <Modal.Body>
           <Container className="projectContainer">
             <Card>
-              <Card.Header>
+            <Card.Body>
+                <Card.Title>A debugging forum app!</Card.Title>
+                <Card.Subtitle><a href="https://github.com/joshlawlor/Debugger-App">Github Repository</a> </Card.Subtitle>
+            </Card.Body>
                 <Carousel>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={bug1} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={bug2} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={bug3} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={bug4} alt="1"></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img src={bug5} alt="1"></img>
                   </Carousel.Item>
                 </Carousel>
-              </Card.Header>
-              <Card.Title>BugBuddy</Card.Title>
             </Card>
           </Container>
         </Modal.Body>
       </Modal>
 
-    {/***************** PROJECT THREE MODAL *****************/}
+      {/***************** PROJECT THREE MODAL *****************/}
 
       <Modal
         className="projectModal"
@@ -181,30 +205,33 @@ function ProjectPage() {
         <Modal.Body>
           <Container className="projectContainer">
             <Card>
-              <Card.Header>
+            <Card.Body>
+                <Card.Title>My first portfolio website</Card.Title>
+                <Card.Subtitle><a href="https://github.com/joshlawlor/Debugger-App">Github Repository</a> </Card.Subtitle>
+            </Card.Body>
                 <Carousel>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={portfolio1} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={portfolio2} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={portfolio3} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={portfolio4} alt="1"></img>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img src={portfolio5} alt="1"></img>
                   </Carousel.Item>
                 </Carousel>
-              </Card.Header>
-              <Card.Title>BugBuddy</Card.Title>
             </Card>
           </Container>
         </Modal.Body>
       </Modal>
 
-    {/***************** PROJECT FOUR MODAL *****************/}
-
+      {/***************** PROJECT FOUR MODAL *****************/}
 
       <Modal
         className="projectModal"
@@ -221,23 +248,24 @@ function ProjectPage() {
         <Modal.Body>
           <Container className="projectContainer">
             <Card>
-              <Card.Header>
+            <Card.Body>
+                <Card.Title>My first group project</Card.Title>
+                <Card.Subtitle><a href="https://github.com/joshlawlor/Debugger-App">Github Repository</a> </Card.Subtitle>
+            </Card.Body>
                 <Carousel>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={movie1} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={movie2} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={movie4} alt="1"></img>
                   </Carousel.Item>
                   <Carousel.Item>
-                    <img src={bugbuddy} alt="1"></img>
+                    <img src={movie3} alt="1"></img>
                   </Carousel.Item>
                 </Carousel>
-              </Card.Header>
-              <Card.Title>BugBuddy</Card.Title>
             </Card>
           </Container>
         </Modal.Body>
