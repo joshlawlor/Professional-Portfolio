f8b428ef664323ba5a2040b4407b38ce09e670f9
Josh Lawlor <joshlawlor1@gmail.com>
2023-01-23 21:26:09 -0500

    All pages finished. Added links from movies and books in about page. Need to resize some icons and footer, then ready for first deploy

--- portfolio-app/src/pages/AboutPage/AboutPage.jsx
+++ portfolio-app/src/pages/AboutPage/AboutPage.jsx
@@ -4,6 +4,14 @@ import "./AboutPage.css";
 import linkedin from "../../assets/images/linkedin.png";
 import github from "../../assets/images/githubicon.png";
 import emailicon from "../../assets/images/email.png";
+import SLAUGHTER from "../../assets/images/slaughter.jpg";
+import thebook from "../../assets/images/thebook.jpg";
+import bigfish from "../../assets/images/bigfish.jpg";
+import newworld from "../../assets/images/newworld.jpg";
+import nope from "../../assets/images/nope.jpg";
+import parasite from "../../assets/images/parasite.jpg";
+import haine from "../../assets/images/haine.jpg";
+import nocountry from "../../assets/images/nocountry.jpg";
 
 function AboutPage() {
   return (
@@ -42,15 +50,50 @@ function AboutPage() {
         </p>
 
         <div className="bookBox">
-          <ul>
-            <li>BOOK</li>
-          </ul>
+          <li>
+          <a href="https://en.wikipedia.org/wiki/Brave_New_World">
+          <img className="bookImg" src={newworld}></img>
+                </a>
+          </li>
+          <li>
+          <a href="https://en.wikipedia.org/wiki/Slaughterhouse-Five">
+          <img className="bookImg" src={SLAUGHTER}></img>
+                </a>
+          </li>
+          <li>
+          <a href="https://www.penguinrandomhouse.com/books/187467/the-book-by-alan-watts/">
+          <img className="bookImg" src={thebook}></img>
+                </a>
+          </li>
+          <li>
+          <a href="https://en.wikipedia.org/wiki/Catching_the_Big_Fish">
+          <img className="bookImg" src={bigfish}></img>
+                </a>
+          </li>
         </div>
 
         <div className="movieBox">
-          <ul>
-            <li>MOVIE</li>
-          </ul>
+          <li>
+          <a href="https://www.imdb.com/title/tt10954984/">
+          <img className="movieImg" src={nope}></img>
+                </a>
+          </li>
+          <li>
+          <a href="https://www.imdb.com/title/tt6751668/">
+          <img className="movieImg" src={parasite}></img>
+
+                </a>
+          </li>
+          <li>
+            <a href="https://www.imdb.com/title/tt0113247/">
+            <img className="movieImg" src={haine}></img>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.imdb.com/title/tt0477348/">
+              <img className="movieImg" src={nocountry}></img>
+            </a>
+          </li>
         </div>
       </div>
     </div>
